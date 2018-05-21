d6a26be97fe932a700d0560e77be72e4854e78b1
Evaline <evalinebai@gmail.com>
2018-05-20 18:49:01 -0700

    Make cv page more responsive - closes #9

--- src/CV.jsx
+++ src/CV.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import './style/cv.css';
 
 const CV = () => (
-  <div className="grid2">
-    <div className="grid-item1">
-      <a
-        href="https://drive.google.com/file/d/0B1hwEpLvdn6tZC04VGFMTGl0ZTg/view"
-        target="_blank"
-      >
-        <h1 className="document link">CV</h1>
-      </a>
-    </div>
-    <div className="grid-item2">
-      <a
-        href="https://drive.google.com/file/d/1rxPSCv1Imw87KPFAHULVTDdsUT6iBABu/view"
-        target="_blank"
-      >
-        <h1 className="document link">RESUME</h1>
-      </a>
-    </div>
+  <div className="cv-container">
+    <Container>
+      <Row>
+        <Col>
+          <div className="grid-item">
+            <a
+              href="https://drive.google.com/file/d/0B1hwEpLvdn6tZC04VGFMTGl0ZTg/view"
+              target="_blank"
+            >
+              <h1 className="document link">CV</h1>
+            </a>
+          </div>
+        </Col>
+        <Col>
+          <div className="grid-item">
+            <a
+              href="https://drive.google.com/file/d/1rxPSCv1Imw87KPFAHULVTDdsUT6iBABu/view"
+              target="_blank"
+            >
+              <h1 className="document link">RESUME</h1>
+            </a>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   </div>
 );
 
